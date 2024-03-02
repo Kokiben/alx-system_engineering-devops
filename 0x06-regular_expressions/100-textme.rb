8a52f6f94a78aefb9aff4570af75210ae1e675b9
Kokiben <bennassarkaoutar@gmail.com>
2024-03-01 18:38:46 -0800

    textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 # print ruby script
-sti = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
+str = ARGV[0].scan(/\[([^\]([TFSu])]*)\]/).join(", ")
 str.split(/\w+:(.*), to:(.*), flags:(.*)/)
-puts sti
+puts str
