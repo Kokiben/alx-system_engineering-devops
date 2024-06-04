bcdfed877bd6304aaf46ebc2595bf012e8424797
Kokiben <bennassarkautar@gmail.com>
2024-06-04 14:51:08 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API"""
+"""function that queries the Reddit API """
 import requests
 
+
 def number_of_subscribers(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'custom_user_agent'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if the subreddit exists and response is OK
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException as e:
-        # In case of any network related errors
+    """returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    if response.status_code != 200:
+        return 0
+    results = response.json().get("data", {})
+    return results.get("subscribers", 0)
+
 
 # Example usage:
-print(number_of_subscribers('python'))  # Should print the number of subscribers for r/python
-print(number_of_subscribers('thissubredditdoesnotexist'))  # Should print 0
+if __name__ == "__main__":
+    print(number_of_subscribers('programming'))  # Should print the number of subscribers for r/programming
+    print(number_of_subscribers('thissubredditdoesnotexist'))  # Should print 0
