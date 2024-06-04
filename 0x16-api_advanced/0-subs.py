67e0ec5c0adcf7f09223f59751d09ba17bfb00f1
Kokiben <bennassarkautar@gmail.com>
2024-06-04 15:01:17 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    if response.status_code != 200:
-        return 0
-    results = response.json().get("data", {})
-    return results.get("subscribers", 0)
+    results = response.json().get("data")
+    return results.get("subscribers")
