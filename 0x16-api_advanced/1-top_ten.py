53feea2e04b6d74ee0365d7bad2a0dbb0ff17865
Kokiben <bennassarkaoutar@gmail.com>
2024-08-13 13:04:37 -0700

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
 """
-this doc for module
+Function that queries the Reddit API
 """
 import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-
 
 def top_ten(subreddit):
-    """method doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
-        print("None")
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = {
+        'limit': 10
+    }
+    req = requests.get(
+        url, headers={'User-Agent': 'Python/requests'}, params=params)
+
+    try:
+        resp = req.json()
+        posts = resp.get("data", {}).get("children", None)
+        if posts is None:
+            print(None)
+        else:
+            [print(post.get("data").get("title")) for post in posts]
+    except Exception:
+        print(None)
