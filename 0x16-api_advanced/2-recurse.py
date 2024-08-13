a829e58397c9299ed429db33b3704f6d61182f6c
Kokiben <bennassarkaoutar@gmail.com>
2024-08-13 13:53:14 -0700

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
 """
-Recursive function that queries the Reddit API and
-returns a list containing the titles of all
-hot articles for a given subreddit
+recursive function that queries the Reddit API
 """
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=None):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    params = {
-        "after": after
+def recurse(subreddit, hot_list=[], pagination_token=None):
+    api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    query_paramt = {
+        "after": pagination_token
     }
-    req = requests.get(
-        url, headers={'User-Agent': 'Python/requests'}, params=params)
+    api_reqt = requests.get(
+        api_url, headers={'User-Agent': 'Python/requests'}, params=query_paramt)
     try:
-        resp = req.json()
-        posts = resp.get("data", {}).get("children", None)
-        after = resp.get("data", {}).get("after", None)
-        if posts is not None:
-            [hot_list.append(post.get("data").get("title")) for post in posts]
-        if after is None:
+        api_rsp = api_reqt.json()
+        article_list = api_rsp.get("data", {}).get("children", None)
+        after = api_rsp.get("data", {}).get("after", None)
+        if article_list is not None:
+            [hot_list.append(article.get("data").get("title")) for article in article_list]
+        if pagination_token is None:
             if len(hot_list) == 0:
                 return None
             return hot_list
         else:
-            return recurse(subreddit, hot_list, after=after)
+            return recurse(subreddit, hot_list, pagination_token=pagination_token)
     except Exception:
         return None
