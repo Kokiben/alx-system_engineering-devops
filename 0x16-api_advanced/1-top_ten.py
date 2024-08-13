b74af3f75182586cbf15642fd5ef97df1ff23c40
Kokiben <bennassarkaoutar@gmail.com>
2024-08-13 11:50:26 -0700

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""Get the top 10 hot posts of a subreddit."""
+"""
+this doc for module
+"""
 import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed for a given subreddit."""
+    """method doc"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'my-app/0.0.1'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code != 200:
-            print(None)
-            return
-        
-        data = response.json().get('data')
-        if data is None:
-            print(None)
-            return
-        
-        posts = data.get('children')
-        if not posts:
-            print(None)
-            return
-        
-        for post in posts:
-            print(post.get('data', {}).get('title'))
-    
-    except requests.RequestException:
-        print(None)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
