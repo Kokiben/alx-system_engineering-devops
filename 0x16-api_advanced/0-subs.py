5b5b5739571772964903997f61b9daa1ed3d8722
Kokiben <bennassarkaoutar@gmail.com>
2024-08-13 13:00:52 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,20 +2,19 @@
 """
 Function that queries the Reddit API
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    return 0.
+    return  the number of subscribers.
     """
-    req = requests.get(
+    api_reqts = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "Custom"},
     )
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    if api_reqts.status_code == 200:
+        return api_reqts.json().get("data").get("subscribers")
     else:
         return 0
