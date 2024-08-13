438d9f9b5a4cbe0a0184a97db5455463cc5422f8
Kokiben <bennassarkaoutar@gmail.com>
2024-08-13 12:32:26 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,19 +4,13 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
+    """returns the number of subscribers."""
+    api_url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    request_headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    # Check if the subreddit is valid
-    if response.status_code != 200:
+    api_response = requests.get(api_url, headers=request_headers, allow_redirects=False)
+    if api_response.status_code == 404:
         return 0
-
-    # Parse the response and get the number of subscribers
-    results = response.json().get("data")
-    if results is None:
-        return 0
-    return results.get("subscribers", 0)
+    data = api_response.json().get("data")
+    return data.get("subscribers")
