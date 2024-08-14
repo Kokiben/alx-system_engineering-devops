f099d0ae9b7bbc91ccfbea53fe03506ba5a114a1
Kokiben <bennassarkaoutar@gmail.com>
2024-08-14 16:44:48 -0700

    pp

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,14 @@
+# strace can attach to a current running process
+# Apache is returning a 500 error.
+
+exec { 'Fixer':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/bin/:/usr/local/bin/:/bin/',
+  onlyif  => "grep 'phpp' /var/www/html/wp-settings.php",
+  notify  => Service['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
